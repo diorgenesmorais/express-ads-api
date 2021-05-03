3187833466578459ff84d395720f060b75533076
Diorgenes Morais <diorgenesmorais@gmail.com>
2021-05-03 14:54:42 -0300

    feat: add dotenv for loads environment variables

--- src/index.js
+++ src/index.js
@@ -5,8 +5,16 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import {router} from './router.js';
 import { startDatabase } from './database/mongdb.js';
+import dotenv from 'dotenv';
+
+dotenv.config({
+    path: process.env.NODE_ENV === "dev" ? ".env.test" : ".env"
+});
 
 const app = express();
+
+const environment = process.env.NODE_ENV;
+const port = process.env.PORT;
 
 app.use(helmet());
 app.use(bodyParser.json());
@@ -15,7 +23,7 @@ app.use(morgan('combined'));
 app.use(router);
 
 startDatabase().then(async () => {
-    app.listen(3001, () => {
-        console.log('Server running on the port 3001');
+    app.listen(port, () => {
+        console.log(`Server running in ${environment} on the port ${port}`);
     });
 });
